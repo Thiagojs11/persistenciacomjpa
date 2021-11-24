1e077f9a552ec5a6c1cef31cafb9f709824584c3
Thiago Jose <tjose@sccalpha.local>
2021-11-24 15:10:41 -0300

    Consultas com filtros

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,10 +1,10 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -13,6 +13,18 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
+		CadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p2 -> System.out.println(p2.getNome()));
+	}
+
+	private static void CadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
 		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 		
@@ -25,8 +37,10 @@ public class CadastroDeProduto {
 		em.flush();
 		em.clear();
 		
-		em.merge(celulares);
+		celulares = em.merge(celulares);
 		celulares.setNome("1234");
+		em.flush();
+		//em.remove(celulares);
 		em.flush();
 	}
 
